511c3a976680cf5a2b8868ca88ce72f3081ebe9e
jonathan2004-git <jonathanpatrick.gouba.25@stud.itsaltoadriatico.it>
2026-06-16 10:14:11 +0200

    Update test_blink.ino

--- hardware/test_blink.ino
+++ hardware/test_blink.ino
@@ -17,3 +17,5 @@ void loop() {
   digitalWrite(13, HIGH);  // D1 spento
   delay(1000);
 }
+------------------------------------------------------
+  questo codice verifica se lo shield è stato installato bene sull'arduino, facendo l'ampeggera il led al secondo 
